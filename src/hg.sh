191eafc9ac70a129f92cf41e914fb0f1f8284646
Nick Hurley <hurley@todesschaf.org>
2011-02-22 14:34:02 -0800

    Add check for python version
    Also, get rid of unused cli args, and add vim modeline

--- src/hg.sh
+++ src/hg.sh
@@ -13,6 +13,29 @@ export PY_GIT_COMMITTER_IDENT="$(git var GIT_COMMITTER_IDENT)"
 export PY_GIT_CONFIG="$(git config -l)"
 export PY_GIT_LIBEXEC="$(dirname $0)"
 
+PYVERSION="$(python -V 2>&1 | cut -d' ' -f2 | cut -d. -f1,2)"
+PYMAJOR="$(echo $PYVERSION | cut -d. -f1)"
+PYMINOR="$(echo $PYVERSION | cut -d. -f2)"
+
+invalid_python() {
+	echo "Python version 2.7 (or higher) or 3.2 (or higher) must be installed"
+	exit 1
+}
+
+if [[ $PYMAJOR -eq 2 ]] ; then
+	if [[ $PYMINOR -lt 7 ]] ; then
+		invalid_python
+	fi
+else
+	if [[ $PYMAJOR -eq 3 ]] ; then
+		if [[ $PYMINOR -lt 2 ]] ; then
+			invalid_python()
+		fi
+	else
+		invalid_python()
+	fi
+fi
+
 case "$1" in
 	clone)
         NONGIT_OK=Yes
@@ -26,6 +49,8 @@ case "$1" in
 		;;
 esac
 
+shift
+
 . git-sh-setup
 
 if [[ -z "$NONGIT_OK" ]] ; then
@@ -36,3 +61,4 @@ fi
 git $GCMD "$@"
 
 exit $?
+# vim: set noexpandtab:
